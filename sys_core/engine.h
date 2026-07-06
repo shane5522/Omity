0620f8aadd91fbbe13f1eb4fcfd947271af3cf1f
KwonHyukTae <kwon02081@naver.com>
2026-07-07 00:10:09 +0900

    크로스플랫폼 백그라운드 상주를 위한 실패 큐 및 상태 변수 정의

---
+++ sys_core/engine.h
@@ -0,0 +1,41 @@
+#ifndef OMITY_ENGINE_H
+#define OMITY_ENGINE_H
+
+#include <string>
+#include <vector>
+#include <thread>
+#include <atomic>
+
+#ifdef _WIN32
+    #include <windows.h>
+#endif
+
+namespace Omity {
+
+class Engine {
+public:
+    Engine();
+    ~Engine();
+
+    bool Initialize();                  
+    void StartBackgroundLoop();         
+    void Stop();                        
+    void MinimizeMemoryUsage();         
+
+private:
+
+    bool IsNetworkConnected();
+    void ProcessFailedQueue();
+
+    std::atomic<bool> m_isRunning;      
+
+#ifdef _WIN32
+    HANDLE m_hDir;                     
+#endif
+
+    std::vector<std::string> m_failedQueue;
+};
+
+} // namespace Omity
+
+#endif // OMITY_ENGINE_H
